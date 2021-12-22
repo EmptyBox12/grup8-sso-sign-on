c8b38fe795597b3eafcdd8998943e26cec6ea510
EmptyBox12 <ataberktumay@gmail.com>
2021-12-22 14:28:21 +0300

    login pages checks auth, if correct redirects

--- sso-auth/api/app.js
+++ sso-auth/api/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const authRoute = require("./routes/authRoute");
+const cors = require('cors')
 const app = express();
 const port = 3001;
 
 //midlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
 
 app.use("/", authRoute)
 
--- sso-auth/client/src/App.js
+++ sso-auth/client/src/App.js
@@ -1,11 +1,54 @@
-import './App.css';
+import { useState } from "react";
+import sha256 from "js-sha256";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    //should get salt from an api
+    let salt = "alotech";
+    let hashedPass = sha256(password + salt);
+    try {
+      let query = window.location.search.substring(1).split("=")[1];
+      let loginData = await axios.post(
+        `http://localhost:3001/isAuthorized/?redirectURL=${query}`,
+        { username: username, password: hashedPass }
+      );
+      let loginInfo = loginData.data;
+      if(loginInfo.status == "success"){
+        window.location.href = query + `?token=${loginInfo.accessToken}`; 
+      }
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  }
+
   return (
     <div className="App">
-      <form>
-        <input type="text" placeholder = "username" />
-        <input type="password" placeholder ="password" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          required
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          required
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
         <button type="submit">Login</button>
       </form>
     </div>
