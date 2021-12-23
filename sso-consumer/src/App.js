c21ef572293ad55ddee6e6caf8953eb175f95ff1
EmptyBox12 <ataberktumay@gmail.com>
2021-12-23 23:35:35 +0300

    consumer css update

--- sso-consumer/src/App.js
+++ sso-consumer/src/App.js
@@ -7,40 +7,50 @@ function App() {
   const [cookies] = useCookies(["accessToken"]);
   const [userInfo, setUserInfo] = useState(null);
 
-
   useEffect(() => {
-      (async function checkCookie() {
-        if (cookies.accessToken) {
-          try {
-            let response = await axios.post(
+    (async function checkCookie() {
+      if (cookies.accessToken) {
+        try {
+          let response = await axios.post(
             `http://localhost:3001/verifyToken/?url=${window.location.href}`,
-              {
-                token: cookies.accessToken,
-              }
-            );
-            if(response.data.status === "success"){
-              setUserInfo({
-                name:"Ataberk",
-                surname:"Tümay",
-                email:"fındık@gmail.com",
-              })
+            {
+              token: cookies.accessToken,
             }
-          } catch (err) {
-            if (err.response.data.status === "fail") {
-              window.location.href = `http://localhost:3000/?redirect=${window.location.href}`;
-            }
+          );
+          if (response.data.status === "success") {
+            setUserInfo({
+              user_id: "1",
+              username: "ataberktumay",
+              user_name: "Ataberk",
+              user_surname: "Tümay",
+              user_email: "fındık@gmail.com",
+              user_type: "admin",
+            });
+          }
+        } catch (err) {
+          if (err.response.data.status === "fail") {
+            window.location.href = `http://localhost:3000/?redirect=${window.location.href}`;
           }
         }
-      })();
+      }
+    })();
   }, []);
 
   return (
     <div className="App">
       {userInfo && (
-        <div>
-          <p>{userInfo.name}</p>
-          <p>{userInfo.surname}</p>
-          <p>{userInfo.email}</p>
+        <div className="userInfoContainer">
+          <div className="title">User Information</div>
+          <div className="idInfo">
+            <span>ID: {userInfo.user_id}</span> <span>{userInfo.username}</span>
+          </div>
+          <div className="userPersonal">
+            <span>{userInfo.user_name}</span> <span>{userInfo.user_surname}</span>
+          </div>
+          <div className="emailInfo">
+            <span>{userInfo.user_email}</span> <span>{userInfo.user_type}</span>
+             
+          </div>
         </div>
       )}
     </div>
